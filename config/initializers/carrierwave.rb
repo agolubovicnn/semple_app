ecb42d75da1b28b321842423bf6e5c8c18b9dae6
Aleksandar Golubovic <agolubovicnn@gmail.com>
2016-06-15 11:00:38 +0200

    Add user microposts

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,13 @@ if Rails.env.production?
 
       config.fog_credentials = {
         :provider               => 'AWS',                        # required
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+        :aws_access_key_id     => ENV['AKIAJX6AOTQHTW32UX6Q'],
+        :aws_secret_access_key => ENV['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 '],
         :region                 => 'eu-central-1',                  # optional, defaults to 'us-east-1'
         :host                   => 's3.example.com',             # optional, defaults to nil
         :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
       }
-      config.fog_directory  = ENV['S3_Bucket']                             # required
+      config.fog_directory  = ENV['aca76']                             # required
       config.fog_public     = false                                   # optional, defaults to true
       config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
     end
